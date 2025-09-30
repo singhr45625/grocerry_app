a50521c6b6d1e05ca5873d45c695f58aa608edd2
rishu2004-rs <rishukumari2144@gmail.com>
2025-09-30 14:28:17 +0530

    fix: Removed app.listen() for Vercel Serverless deployment

--- server/server.js
+++ server/server.js
@@ -34,8 +34,5 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3001;
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
-};
 
 module.exports = app; 
